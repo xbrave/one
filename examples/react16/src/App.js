c1bf0da59be8fe8f7255c82f854a1c6c87779a6f
xbrave <sunzy1993@hotmail.com>
2020-09-08 16:57:07 +0800

    update react & vue project

--- examples/react16/src/App.js
+++ examples/react16/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { Divider } from 'antd';
+
+import 'antd/dist/antd.min.css';
 import './App.css';
 
-function App() {
+import LibVersion from './components/LibVersion';
+import HelloModal from './components/HelloModal';
+
+import Home from './pages/Home';
+const About = lazy(() => import('./pages/About'));
+
+const RouteExample = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router basename={window.__POWERED_BY_QIANKUN__ ? '/react16' : '/'}>
+      <nav>
+        <Link to="/">Home</Link>
+        <Divider type="vertical" />
+        <Link to="/about">About</Link>
+      </nav>
+      <Suspense fallback={null}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="app-main">
+      <LibVersion />
+      <HelloModal />
+
+      <Divider />
+
+      <RouteExample />
     </div>
   );
 }
-
-export default App;
